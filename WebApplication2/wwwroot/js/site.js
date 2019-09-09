52cd9243e81cbc939ca4b6529b4b7d15561c986e
Sharp <1349096875@qq.com>
2019-09-09 21:10:49 +0800

    第一次提交

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+//利用JavaScript计算出：[(23+7)x12-8]÷6的值
+var i = 0
+i = [(23 + 7) * 12 - 8] / 6
+console.log(i);
+
+
+
+//利用循环，计算出2 + 4 + 6 + 8 +...+100=?
+var sum = 0
+for (var i = 2; i <= 100; i = i + 2) {
+    sum += i
+}
+console.log(sum);
+
+
