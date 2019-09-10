6c33b27eb764542d789a7f1a2528555be8aa0807
ZJQ <1349096875@qq.com>
2019-09-10 19:27:32 +0800

    第三次提交

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -11,9 +11,26 @@ console.log(i);
 
 //利用循环，计算出2 + 4 + 6 + 8 +...+100=?
 var sum = 0
-for (var i = 2; i <= 100; i = i + 2) {
+for (var i = 0;  i = i + 2; i <= 100) {
     sum += i
 }
 console.log(sum);
 
 
+//将源栈同学姓名/昵称装入数组，再根据该数组输出所有同学，以及共有多少位同学
+
+
+var students = ['A', 'B', 'C', 'D',];
+arr = students
+console.log(arr, length);
+
+
+//如果一个数只能被1和它自己整除，这个数就被称之为“素数”。请找出100以内的所有素数。
+var zhishu = i,
+
+for (i = 4; i++; i <= 100;)
+if (i / 2%=== 0) {i=zhizhu
+    
+}
+console.log('1', '2', '3');
+console.log(zhishu);
