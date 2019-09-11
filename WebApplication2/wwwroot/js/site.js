2fbd52f77530f613500f8170066fbc0ad139b451
ZJQ <1349096875@qq.com>
2019-09-11 19:28:59 +0800

    9.11

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -3,14 +3,14 @@
 
 // Write your Javascript code.
 //利用JavaScript计算出：[(23+7)x12-8]÷6的值
-var i = 0
+var i = 0;
 i = [(23 + 7) * 12 - 8] / 6
 console.log(i);
 
 
 
 //利用循环，计算出2 + 4 + 6 + 8 +...+100=?
-var sum = 0
+var sum = 0;
 for (var i = 0;  i = i + 2; i <= 100) {
     sum += i
 }
@@ -26,11 +26,69 @@ console.log(arr, length);
 
 
 //如果一个数只能被1和它自己整除，这个数就被称之为“素数”。请找出100以内的所有素数。
-var zhishu = i,
-
-for (i = 4; i++; i <= 100;)
-if (i / 2%=== 0) {i=zhizhu
-    
+var i;
+for (i = 3; i++; i <= 100)
+    if (i % 2 === 0) {
+        i = zhizhu  
 }
-console.log('1', '2', '3');
-console.log(zhishu);
+console.log(zhishu , '1' , '2' , '3');
+
+
+//独立完成“冒泡排序”算法
+
+
+//将之前“找出素数”的代码封装成一个函数findPrime(max)，可以打印出max以内的所有素数。
+
+for (var i = 3; i++; i <= 100)
+    if (i % 2 === 0) {
+        i = zhizhu
+    }
+console.log(zhishu , '1' , '2' , '3');
+function findPrime(max) {
+    alert.zhishu = i
+    alert.findPrime(max) = function () {
+        return alert.zhishu
+    }
+}
+var findPrime = new findPrime(max);
+alert(findPrime.i);
+alert(findPrime.max);
+return console.log(max);
+
+
+//自行设计参数，将之前“累加求和”的代码封装成一个函数Sum()，可以计算任意起始位置、任意步长（如：1,3,5……或者0,5,10,15……）的等差数列之和。
+
+var sum = 0;
+for (var i = 0; i = i + 2; i <= 100) {
+    sum += i
+}
+console.log(sum);
+function sum() {
+    for (j = 0; j++; j <= 100) {
+        sum+=j+"num"
+    }
+    return console.log(sum);
+}
+
+
+
+
+//封装一个函数Swap(arr, i, j)，可以交换数组arr里下标 i 和 j 的值
+functionSwap(arr){
+    var i, j;
+    arr[i, j];
+    arr.reverse();
+}
+console.log(arr);
+
+
+//删除一个数组里面重复的元素
+
+    var arr[];    
+    for (var i = 0; i < this.length; i++) {    
+        if (arr1.indexOf(this[i]) == -1) {
+            arr.push(this[i]);
+        }
+    return console.log(arr);
+}
+
