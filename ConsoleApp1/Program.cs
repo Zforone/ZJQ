5cf5ce820851ab526dbc749753a109b8faeec1d4
ZJQ <1349096875@qq.com>
2019-11-07 16:31:08 +0800

    C#第一次作业

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "曾俊清";
+            string clause = "爱学习，一天不学习就浑身难受！";
+
+            Console.WriteLine(name + clause);
+            Console.ReadLine();
+
+        }
+    }
+}
