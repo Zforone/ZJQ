7b8b71759090a0df73761c05ae56b0e6894adf5d
ZJQ <1349096875@qq.com>
2019-12-04 09:58:54 +0800

    修改上一次的BUG

--- ConsoleApp1/TokenManager.cs
+++ ConsoleApp1/TokenManager.cs
@@ -5,22 +5,29 @@ using System.Text;
 namespace ConsoleApp1
 {
     //声明一个令牌管理（TokenManager）类：
-        //使用私有的Token枚举_tokens存储所具有的权限
-        //暴露Add(Token)、Remove(Token)和Has(Token)方法，可以添加、删除和查看其权限
+    //使用私有的Token枚举_tokens存储所具有的权限
+    //暴露Add(Token)、Remove(Token)和Has(Token)方法，可以添加、删除和查看其权限
     internal class TokenManager
     {
         private Token _tokens { get; set; } /*= Token.SuperAdmin ;*/
-        public void Add( Token token)
-        {            
-            _tokens = _tokens | token; 
-        }
-        public void Remove( Token token)
+        public void Add(Token token)
         {
+            _tokens = _tokens | token;
+        }
+        public void Remove(Token token)
+        {
+            if ((_tokens & token) ==token)
+            {  
                 _tokens = _tokens ^ token;
+            }
+            else
+            {
+                Console.WriteLine("没有此权限！");
+            }           
         }
         public Token Has()
-        {            
-            return _tokens;            
+        {
+            return _tokens;
         }
-    }     
+    }
 }
