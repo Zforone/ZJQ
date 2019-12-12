ff42f78e7fa2e61515e538ba5fd92ff7bd1718ea
ZJQ <1349096875@qq.com>
2019-12-12 21:40:58 +0800

    一个评论必须有一个它所评论的文章

--- ConsoleApp1/17bang/Comment.cs
+++ ConsoleApp1/17bang/Comment.cs
@@ -7,5 +7,11 @@ namespace ConsoleApp1._17bang
     //评论（Comment）类
     class Comment
     {
+        //一个评论必须有一个它所评论的文章
+        public Comment(Article article)
+        {
+
+        }
+        public Article Article { get; set; }
     }
 }
