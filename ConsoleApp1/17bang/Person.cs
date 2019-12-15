41b4283b2648893bca994fd44cdc5350e953cf95
ZJQ <1349096875@qq.com>
2019-12-15 20:54:06 +0800

    声明委托（ProvideWater）

---
+++ ConsoleApp1/17bang/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1._17bang
+{
+    class Person
+    {
+    }
+    //声明一个委托：打水（ProvideWater），可以接受一个Person类的参数，返回值为int类型 
+    delegate int ProvideWater(Person person);
+}
