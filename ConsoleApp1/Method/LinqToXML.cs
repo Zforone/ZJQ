2302fe979c1f9beec65521fc8e5376e8ac3f4c3e
ZJQ <1349096875@qq.com>
2019-12-21 20:13:15 +0800

    在根节点下添加一个新的article元素

--- ConsoleApp1/Method/LinqToXML.cs
+++ ConsoleApp1/Method/LinqToXML.cs
@@ -39,10 +39,30 @@ namespace ConsoleApp1.Method
                     new XElement("authorId", "1")
                     ));
             Console.WriteLine(luckystack);
-
+            
             //以文件形式存放到磁盘中
             XDocument document = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), luckystack);
             document.Save("E://XML.xml");
+
+            Console.WriteLine();
+            Console.WriteLine("-----分界线-----");
+            Console.WriteLine();
+
+
+            //在根节点下添加一个新的article元素，内容至少包含id、title和authorId 
+            luckystack.Add(new XElement("article", new XAttribute("isDraft", true),
+                            new XElement("id", "88"),
+                            new XElement("title", "源栈"),
+                            new XElement("authorId", "88")
+                                        )
+                            );
+            luckystack.Save("E:\\17bang.xml");
+            Console.WriteLine(luckystack);
+            //删除id = 12的评论
+            //改变id = 2的article：isDraft = false，title = 源栈培训：C#进阶-8：异步和并行
+
+
+
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -82,11 +82,13 @@ namespace ConsoleApp1
 
             //UsedLinq.Do();
 
-            //LinqToXML.GetXML();
+            LinqToXML.GetXML();
 
             //再从磁盘中读取到内存中
-            XElement element = XElement.Load("E://XML.xml");
-            Console.WriteLine(element);
+            //XElement element = XElement.Load("E://XML.xml");
+            //Console.WriteLine(element);
+
+
 
             Console.ReadLine();
         }
