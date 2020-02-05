410a6d31f5658529697360ed342cd5cc4532b767
ZJQ <1349096875@qq.com>
2020-02-05 19:05:27 +0800

    声明一个自定义的TagHelper：DateTimeTagHelper ,并生成HTML 标签

---
+++ WebApplicationCore/Pages/PageHelper/DateTimeTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationCore.Pages.PageHelper
+{
+    [HtmlTargetElement("small", Attributes = "pageIndex, path")]
+    public class DateTimeTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";   //原生标签名
+
+            //分别取出pageIndex和path的值
+            object pageIndex = context.AllAttributes["pageIndex"].Value;
+            //output.Attributes.Remove(context.AllAttributes["pageIndex"]);
+
+            object path = context.AllAttributes["path"].Value;
+            //output.Attributes.RemoveAll("path");
+
+            //设置a标签里href的值
+            output.Attributes.Add("href", $"{path}/Page-{pageIndex}");
+            base.Process(context, output);
+        }
+    }
+}
